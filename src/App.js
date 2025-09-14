39781aaf799b9ffc245746375f4075d4c3d48604
Baswal Ganesh <baswalganesh1@gmail.com>
2025-09-14 17:56:43 +0530

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
+
+
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero />
+     
     </>
     
     
